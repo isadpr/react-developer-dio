1bd3d16addc1f99044161c176419b9318f22b4ec
Isabele Rocha <isabeledpr@gmail.com>
2024-03-26 13:04:59 -0300

    projeto dio: header finalizado

---
+++ projeto-dio/dio/src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { Home } from './pages/home'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto-dio/dio/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+
+        </div>
+    )
+}
+
+export  { Home };
